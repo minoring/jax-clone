a905c50625f261a452d222347d8422ebad5090f8
minoring <minho1350@gmail.com>
2020-03-19 21:34:50 -0700

    Implement AbstractValue in core

---
+++ jax/core.py
@@ -0,0 +1,13 @@
+class AbstractValue(object):
+  __slots__ = []
+
+  def at_least_vspace(self):
+    assert False
+
+  def __repr__(self):
+    try:
+      kv_pairs = ('{}={}'.format(k, v) for k, v in self.__dict__.items())
+      return '{}({})'.format(self.__class__.__name__, ','.join(kv_pairs))
+    except AttributeError:
+      return self.__class__.__name__
+  
