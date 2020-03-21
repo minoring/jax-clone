d070b9f42db592d251b8b0a40bc7c7b9bc00896d
minoring <minho1350@gmail.com>
2020-03-20 20:52:04 -0700

    Implement tracer

--- jax/core.py
+++ jax/core.py
@@ -1,3 +1,6 @@
+from collections import namedtuple
+
+
 class AbstractValue(object):
   __slots__ = []
 
@@ -45,3 +48,97 @@ class Primitive(object):
   def impl(self, *args, **kwargs):
     raise NotImplementedError("Evaluation rule for '{}' not implemented".format(
         self.name))
+
+
+class Tracer(object):
+  __array_priority__ = 1000
+  __slots__ = ['trace']
+
+  def __array__(self):
+    raise Exception("Tracer can't be used with raw numpy functions. "
+                    "You might have\n import numpy as np\n instead of \n import jax.numpy as np")
+
+  def __init__(self, trace):
+    self.trace = trace
+
+  def __iter__(self):
+    return iter(self.aval_iter(self))
+
+  def __len__(self):
+    return len(list(iter(self)))
+
+  @property
+  def aval(self):
+    assert False
+
+  def __neg__(self): return self.aval_neg(self)
+  def __eq__(self, other): return self.aval._eq(self, other)
+  def __ne__(self, other): return self.aval._ne(self, other)
+  def __lt__(self, other): return self.aval._lt(self, other)
+  def __le__(self, other): return self.aval._le(self, other)
+  def __gt__(self, other): return self.aval._gt(self, other)
+  def __ge__(self, other): return self.aval._ge(self, other)
+  def __abs__(self): return self.aval._abs(self)
+  def __add__(self, other): return self.aval._add(self, other)
+  def __radd__(self, other): return self.aval._radd(self, other)
+  def __sub__(self, other): return self.aval._sub(self, other)
+  def __rsub__(self, other): return self.aval_rsub(self, other)
+  def __mul__(self, other): return self.aval._mul(self, other)
+  def __rmul__(self, other): return self.aval._rmul(self, other)
+  def __div__(self, other): return self.aval._div(self, other)
+  def __rdiv__(self, other): return self.aval._rdiv(self, other)
+  def __truediv__(self, other): return self.aval._truediv(self, other)
+  def __floordiv__(self, other): return self.aval._floordiv(self, other)
+  def __rfloordiv__(self, other): return self.aval._rfloordiv(self, other)
+  def __divmod__(self, other): return self.aval._divmod(self, other)
+  def __rdivmod__(self, other): return self.aval._rdivmod(self, other)
+  def __mod__(self, other): return self.aval._mod(self, other)
+  def __rmod__(self, other): return self.aval._rmod(self, other)
+  def __pow__(self, other): return self.aval._pow(self, other)
+  def __rpow__(self, other): return self.sval._rpow(self, other)
+  def __matmul__(self, other): return self.aval._matmul(self, other)
+  def __rmatmul__(self, other): return self.aval._rmatmul(self, other)
+  def __and__(self, other): return self.aval._and(self, other)
+  def __rand__(self, other): return self.aval._rand(self, other)
+  def __or__(self, other): return self.aval._or(self, other)
+  def __ror__(self, other): return self.aval._ror(self, other)
+  def __xor__(self, other): return self.aval._xor(self, other)
+  def __invert__(self, other): return self.aval_invert(self, other)
+  def __lshift__(self, other): return self.aval._lshift(self, other)
+  def __rshift__(self, other): return self.aval._rshift(self, other)
+  def __getitem__(self, idx): return self.aval._getitem(self, idx)
+  def __nonzero__(self): return self.aval._nonzero(self)
+  def __bool__(self): return self.aval._bool(self)
+  def __float__(self): return self.aval._float(self)
+  def __int__(self): return self.aval._int(self)
+  def __long__(self): return self.aval._long(self)
+  def __complex__(self): return self.aval._complex(self)
+  def __hex__(self): return self.aval._hex(self)
+  def __oct__(self): return self.aval._oct(self)
+
+  def __getattr__(self, name):
+    # If the aval property raises an AttributeError, get caught here.
+    assert skip_checks or name != 'aval'
+
+    try:
+      attr = getattr(self.aval, name)
+    except KeyError:
+      raise AttributeError(
+          "{} has no attribute {}".format(self.__class__.__name__, name))
+    else:
+      t = type(attr)
+      if t is aval_property:
+        return attr.fget(self)
+      elif t is aval_method:
+        return types.MethodType(attr.fun, self, None)
+      else:
+        return attr
+
+  def __repr__(self):
+    return "Traced<{}>with<{}>".format(self.aval, self.trace)
+
+
+# These can be used to set up forwarding of properties and instance methods from
+# Tracer instances to the underlying avals
+aval_property = namedtuple("aval_property", ["fget"])
+aval_method = namedtuple("aval_method", ["fun"])
